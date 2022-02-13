b7f887c16a8f39107752eade29fe579822090ea9
Nicola <nicola.mcfadden@dcmail.ca>
2022-02-13 11:42:28 -0500

    Added custom button types

--- themes/bb/assets/js/block-editor.js
+++ themes/bb/assets/js/block-editor.js
@@ -8,9 +8,39 @@ wp.blocks.registerBlockStyle( 'core/image', {
     label: 'Boxed Image',
 } );
 
+wp.blocks.registerBlockStyle( 'core/media-text', {
+    name: 'boxed-media-text',
+    label: 'Boxed Media Text',
+} );
+
+wp.blocks.registerBlockStyle( 'core/video', {
+    name: 'boxed-video',
+    label: 'Boxed Video',
+} );
+
 wp.blocks.registerBlockStyle( 'core/table', {
     name: 'striped',
     label: 'Striped Table',
+} );
+
+wp.blocks.registerBlockStyle( 'core/button', {
+    name: 'secondary',
+    label: 'Secondary Button',
+} );
+
+wp.blocks.registerBlockStyle( 'core/button', {
+    name: 'tertiary',
+    label: 'Tertiary Button',
+} );
+
+wp.blocks.registerBlockStyle( 'core/group', {
+    name: 'collage-centered',
+    label: 'Collage Group Centered',
+} );
+
+wp.blocks.registerBlockStyle( 'core/group', {
+    name: 'collage-left',
+    label: 'Collage Group Left',
 } );
 
 wp.blocks.registerBlockStyle( 'core/heading', {
@@ -22,5 +52,7 @@ wp.domReady( function () {
     // reference slug to remove block
     // "core/quote" is data-type attributec
     wp.blocks.unregisterBlockStyle( 'core/image', 'rounded' );
+    wp.blocks.unregisterBlockStyle( 'core/button', 'fill' );
+    wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
     // wp.blocks.unregisterBlockStyle( 'core/quote', 'plain' );
 } );
