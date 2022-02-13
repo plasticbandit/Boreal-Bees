00b6e66108ab77a05f9102ba4d9fedbc232c498e
Nicola <nicola.mcfadden@dcmail.ca>
2022-02-12 23:02:45 -0500

    Enqueued app.css

--- themes/bb/functions.php
+++ themes/bb/functions.php
@@ -131,6 +131,10 @@ add_action( 'widgets_init', 'bb_widgets_init' );
 function bb_scripts() {
 	// !CHANGE TO APP.CSS
 	wp_enqueue_style( 'bb-style', get_stylesheet_uri(), array(), BB_VERSION );
+	wp_enqueue_style( 
+        'app-style',
+        get_template_directory_uri() . '/assets/css/app.css'
+    );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
