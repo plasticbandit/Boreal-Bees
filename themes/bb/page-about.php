0763e3623341765c7b801dc46b629525a026d6ac
Nicola <nicola.mcfadden@dcmail.ca>
2022-02-14 10:55:59 -0500

    Created about page template

---
+++ themes/bb/page-about.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: About Page
+ * 
+ * The template for displaying about pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Boreal_Bees
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+    
+<!-- insert social widgets -->
+<?php
+get_sidebar( 'connect' );
+get_footer();
