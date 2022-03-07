3a18662b875fbac8829726b438007c075852c77c
Nicola <nicola.mcfadden@dcmail.ca>
2022-03-07 16:26:21 -0500

    Updated home page structure

--- themes/bb/page-home.php
+++ themes/bb/page-home.php
@@ -13,12 +13,15 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+	<!-- <section class="home-banner">
+		<?php the_post_thumbnail( ) ?>
+	</section> -->
 
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			get_template_part( 'template-parts/content-home', 'page' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
