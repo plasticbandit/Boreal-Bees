4eadae1ed865d9bdb8eecec4289f1cfa43e91d46
Nicola <nicola.mcfadden@dcmail.ca>
2022-03-06 20:41:40 -0500

    Added woocommerce theme support

--- themes/bb/functions.php
+++ themes/bb/functions.php
@@ -97,8 +97,16 @@ function bb_setup() {
 	*/
 
 	add_theme_support( 'wp-block-nicola-gutenberg-composite-title-block' );
+
 }
 add_action( 'after_setup_theme', 'bb_setup' );
+
+function bb_add_woocommerce_support() {
+	add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'bb_add_woocommerce_support' );
+
+remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0);
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
