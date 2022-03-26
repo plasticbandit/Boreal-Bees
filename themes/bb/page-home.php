59a4e500ce1492d9bddb5d6f608ce4de6392db39
Nicola <nicola.mcfadden@dcmail.ca>
2022-03-26 17:58:26 -0400

    added class for custom styling on home page

--- themes/bb/page-home.php
+++ themes/bb/page-home.php
@@ -12,7 +12,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main site-home">
 	<!-- <section class="home-banner">
 		<?php the_post_thumbnail( ) ?>
 	</section> -->
