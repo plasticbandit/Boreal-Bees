4a31450c3ee3d9447dce326e7d8aa83b5d1f1eb2
Nicola <nicola.mcfadden@dcmail.ca>
2022-03-27 17:20:21 -0400

    Created loop for event archive page

--- themes/bb/archive-event.php
+++ themes/bb/archive-event.php
@@ -9,6 +9,7 @@
 
 get_header();
 ?>
+<?php if( function_exists( 'aioseo_breadcrumbs' ) ) aioseo_breadcrumbs(); ?><!-- .breadcrumbs -->
 
 	<main id="primary" class="site-main">
 
@@ -22,47 +23,35 @@ get_header();
 			</header><!-- .page-header -->
 
 			<?php
-				$args = array(
-					'post_type' => 'event'
-				);
-				$events = new WP_Query( $args );
-				if( $events->have_posts() ) {
-					while( $events->have_posts() ) {
-					$events->the_post();
-					$event_date_box = get_post_meta( get_the_ID(), 'event_date_box', true );
-					$event_permalink = esc_url( get_permalink() );
-					?>
-					<a class="link-no-style" href='<?php echo($event_permalink) ?>' rel="bookmark">
-						<article class="event-list-item">
-							<div class="event-thumb">
-								<?php
-									if(has_post_thumbnail()){
-										the_post_thumbnail();}
-									}
-								?>
-							</div>
-							<div class="event-info">
-								<h2><?php the_title() ?></h2>
-								<h3><?php echo($event_date_box) ?></h3>
-								<div class='content'>
-								<?php the_content() ?>
-								</div>
-							</div>
-						</article>
-					</a>
-						
-					<?php
-					
-				}
-				else {
-					echo 'No events to show.';
-				}
+			/* Start the Loop */
+			$args = array(
+				'post_type' => 'event'
+			);
+			// get events type post
+			$events = new WP_Query( $args );
+			if( $events->have_posts() ) :
+				while( $events->have_posts() ) :
+				$events->the_post();
+				
 
-			the_posts_navigation();
+				/*
+				 * Include the Post-Type-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content-event', get_post_type() );
+				
 
+				endwhile;
+
+				the_posts_navigation();
+
+			endif;
+				
 		else :
 
 			get_template_part( 'template-parts/content', 'none' );
+			echo 'No events to show.';
 
 		endif;
 		?>
@@ -70,5 +59,4 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-// get_sidebar();
 get_footer();
