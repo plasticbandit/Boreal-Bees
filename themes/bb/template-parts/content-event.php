5de695aa1b4269bc9f9283a75df83f2d7dd5306e
Nicola <nicola.mcfadden@dcmail.ca>
2022-03-27 17:21:58 -0400

    Fixed content of events page + loop

--- themes/bb/template-parts/content-event.php
+++ themes/bb/template-parts/content-event.php
@@ -14,62 +14,103 @@
 		<?php
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
+			if ( 'event' === get_post_type() ) :
+				?>
+				<div class="entry-meta">
+					<?php
+					the_category( ', ' );
+					echo get_the_term_list( get_the_ID(), 'event_category' );
+					?>
+				</div><!-- .entry-meta -->
+				<h1 class="post-entry-title">
+					<?php
+					the_title();
+					?>
+				</h1><!-- .entry-title -->
+				<h3>
+					<?php
+					echo get_post_meta( get_the_ID(), 'event_date_box', true );
+					?>
+				</h3><!-- .entry-date -->
+				<div class="post-thumbnail">
+					<?php if(has_post_thumbnail()){
+						the_post_thumbnail();} ?>
+				</div><!-- .entry-thumbnail -->
+			<?php endif; 
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
-		if ( 'event' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				the_category( ', ' );
-				echo get_the_term_list( get_the_ID(), 'event_category' );
-				?>
-			</div><!-- .entry-meta -->
-			<h1 class="post-entry-title">
-				<?php
-				the_title();
-				?>
-			</h1><!-- .entry-title -->
-			<h3>
-				<?php
-				echo get_post_meta( get_the_ID(), 'event_date_box', true );
-				?>
-			</h3><!-- .entry-date -->
-			<div class="post-thumbnail">
-				<?php if(has_post_thumbnail()){
-					the_post_thumbnail();} ?>
-			</div><!-- .entry-thumbnail -->
-		<?php endif; ?>
+		?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content blog-entry-content">
+	
+	<?php
+	if (is_archive(  )) :
+			$event_date_box = get_post_meta( get_the_ID(), 'event_date_box', true );
+			$event_permalink = esc_url( get_permalink() );
+			?>
+			<a class="link-no-style" href='<?php echo($event_permalink) ?>' rel="bookmark">
+				<article class="event-list-item">
+					<div class="event-thumb">
+						<?php
+							if(has_post_thumbnail()){
+								the_post_thumbnail();}
+							
+						?>
+					</div><!-- .entry-thumbnail -->
+					<div class="event-info">
+						<h2><?php the_title() ?></h2><!-- .entry-title -->
+						<h3><?php echo($event_date_box) ?></h3><!-- .entry-date -->
+						<div class='content'>
+						<?php the_content(
+							sprintf(
+								wp_kses(
+									/* translators: %s: Name of current post. Only visible to screen readers */
+									__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'bb' ),
+									array(
+										'span' => array(
+											'class' => array(),
+										),
+									)
+								),
+								wp_kses_post( get_the_title() )
+							)
+						) ?>
+						</div>
+					</div>
+				</article>
+			</a>
 		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'bb' ),
-					array(
-						'span' => array(
-							'class' => array(),
+				
+		
+		else :
+			?>
+			<div class="entry-content blog-entry-content">
+				<?php the_content(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'bb' ),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
 						),
+						wp_kses_post( get_the_title() )
 					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
+				);
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bb' ),
-				'after'  => '</div>',
-			)
-		);
+				wp_link_pages(
+					array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bb' ),
+						'after'  => '</div>',
+					)
+				); ?>
+			</div><!-- .entry-content -->
+		<?php endif;
+		
 		?>
-	</div><!-- .entry-content -->
-
-	<!-- <footer class="entry-footer">
-		<?php bb_entry_footer(); ?>
-	</footer>.entry-footer -->
+		
 </article><!-- #post-<?php the_ID(); ?> -->
