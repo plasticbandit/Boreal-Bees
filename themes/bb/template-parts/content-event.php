68bcba379da8cca619e33e62951f57798a03c72b
Nicola <nicola.mcfadden@dcmail.ca>
2022-03-27 14:30:54 -0400

    Created event content template part

---
+++ themes/bb/template-parts/content-event.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Boreal_Bees
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header blog-entry-header">
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
+
+		if ( 'post' === get_post_type() ) :
+			?>
+			<div class="entry-meta">
+				<?php
+				the_category( ', ' );
+				// bb_posted_on();
+				// bb_posted_by();
+				?>
+			</div><!-- .entry-meta -->
+			<h1 class="post-entry-title">
+				<?php
+				the_title();
+				?>
+			</h1><!-- .entry-title -->
+			<p class="post-author">
+				By
+			<?php
+				the_author();
+				?>
+			</p><!-- .entry-author -->
+			<?php bb_post_thumbnail(); ?>
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content blog-entry-content">
+		<?php
+		the_content(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'bb' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				wp_kses_post( get_the_title() )
+			)
+		);
+
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bb' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+	</div><!-- .entry-content -->
+
+	<!-- <footer class="entry-footer">
+		<?php bb_entry_footer(); ?>
+	</footer>.entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
